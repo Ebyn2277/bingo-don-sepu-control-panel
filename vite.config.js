36d16c8ab8e63292a92e66fa4347019385819ae8
Brayan Presiga <presigabrayan3@gmai.com>
2025-07-21 20:02:19 -0500

    Update vite.config.js to host app

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/bingo-don-sepu-control-panel",
   server: {
     host: "0.0.0.0",
     port: 5173,
